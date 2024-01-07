3cee6fcd8b7cd08619115971a8a6e4e88ee21d97
ElisabethKAndersson <>
2024-01-07 23:58:55 +0000

    Add first app, index page and env.py with secret key

--- Testing log.md
+++ Testing log.md
@@ -1,1 +1,5 @@
 TEST: 
+Tried to check that the index.html is working. Problem with secret key when only in env.py
+    - Had forgot to add: if os.path.isfile("env.py"):
+    import env to settings. Fixed problem.
+
--- senior_fitness/settings.py
+++ senior_fitness/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&387!3i5fw8t3h&+npu-y=54(6)ioamg6(hg%_&v!*s#-090v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sixtyfiveplus',
 ]
 
 MIDDLEWARE = [
